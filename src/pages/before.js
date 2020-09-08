7e76c80fc52d0434f2ecc46f092870c6e24e5b5d
Roshin Kadanna Pally <roshinpally@gmail.com>
2020-09-07 21:34:50 -0400

    text updates

--- src/pages/before.js
+++ src/pages/before.js
@@ -9,17 +9,17 @@ const Before = () => (
         <SEO title="Before" />
         <section class="content">
             <h3>My Past Projects</h3>
-            <p>I worked on developing a new generation of Scopes for the MATLAB and Simulink product family. The new Scopes are performant, extensible, and share a common infrastructure. They are developed using C++, Java, MATLAB, and MATLAB Graphics. Shown below are some of the highlights.
+            <p>I worked on developing a new generation of Scopes for the MATLAB and Simulink product families. The new Scopes are performant, extensible, and share a common infrastructure. C++, Java, MATLAB, and MATLAB Graphics are used for their development. Shown below are some of the highlights.
             </p>
             <h4>Time Scope</h4>
-            <p>A Scope for visualizing time domain signals produced by MATLAB or Simulink simulation.</p>
+            <p>Use this Scope for visualizing time-domain signals produced by MATLAB or Simulink simulation.</p>
             <div>
                 <img src={timeScope} alt="Time Scope"/>
                 <iframe src="https://www.youtube.com/embed/oOPTzvtXDH4" title="New Scopes Interface">
                 </iframe>
             </div>
             <h4>Unified Scopes</h4>
-            <p>Scopes built using a common infrastructure makes development efficient and produces a uniform user interface. Shown below are various Scopes built from a unified infrastructure visualizing a LTE downlink signal.</p>
+            <p>Scopes built using a shared infrastructure makes development efficient and result in a uniform user interface. Shown below are various Scopes built from a unified infrastructure visualizing a LTE downlink signal.</p>
             <div>
                 <img src={lteDownlink} alt="LTE Downlink Signal"/>
             </div>
--- src/pages/now.js
+++ src/pages/now.js
@@ -5,6 +5,16 @@ import SEO from "../components/seo"
 const Now = () => (
     <Layout>
         <SEO title="Now" />
+        <section class="content">
+            <h3>My Current Projects</h3>
+            <p>
+                I currently work on designing and implementing simulation and visualization features for automated driving and signal processing fields. Shown below are some of the highlights.
+            </p>
+            <h4>Driving Scenario Simulation</h4>
+            <p>
+                Driving Scenario Simulation tools enable engineers to design and test their algorithms in MATLAB and Simulink. Key features include: Lidar point cloud generation; low-poly actor meshes; reverse motion in driving scenarios; road network data model for geographic maps such as HERE/OSM and OpenDRIVE; Driving Scenario Designer app; lanes ground truth specification, visualization, and detection; development of a Driving Scenario Reader block and Bird's-Eye Scope in Simulink.
+            </p>
+        </section>
     </Layout>
 )
 
