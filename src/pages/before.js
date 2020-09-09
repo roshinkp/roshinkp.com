22dc924a1798c3fe9cfe3510651d961e9433b156
Roshin Kadanna Pally <roshinpally@gmail.com>
2020-09-08 20:53:07 -0400

    now page

--- src/pages/before.js
+++ src/pages/before.js
@@ -3,20 +3,30 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import timeScope from "../images/timeScope.png"
 import lteDownlink from "../images/lteDownlink.png"
+import logicAnalyzer from "../images/logicAnalyzer.png"
 
 const Before = () => (
     <Layout>
         <SEO title="Before" />
         <section class="content">
             <h3>My Past Projects</h3>
-            <p>I worked on developing a new generation of Scopes for the MATLAB and Simulink product families. The new Scopes are performant, extensible, and share a common infrastructure. C++, Java, MATLAB, and MATLAB Graphics are used for their development. Shown below are some of the highlights.
+            <p>I worked on developing a new generation of Scopes for the MATLAB and Simulink product families. The new Scopes are performant, extensible, and share a common infrastructure. I used C++, JavaScript, Java, MATLAB, and MATLAB Graphics for development. Shown below are some of the highlights.
             </p>
+            <h4>Logic Analyzer</h4>
+            <p>
+                Visualize transitions, timing information, and states in a signal using a Logic Analyzer. 
+            </p>
+            <div class="highlights">
+                <div><img src={logicAnalyzer} alt="Logic Analyzer"/></div>
+                <div><iframe src="https://www.youtube.com/embed/QvNkOvvuPp0" title="Logic Analyzer">
+                </iframe></div>
+            </div>
             <h4>Time Scope</h4>
             <p>Use this Scope for visualizing time-domain signals produced by MATLAB or Simulink simulation.</p>
-            <div>
-                <img src={timeScope} alt="Time Scope"/>
-                <iframe src="https://www.youtube.com/embed/oOPTzvtXDH4" title="New Scopes Interface">
-                </iframe>
+            <div class="highlights">
+                <div><img src={timeScope} alt="Time Scope"/></div>
+                <div><iframe src="https://www.youtube.com/embed/oOPTzvtXDH4" title="New Scopes Interface">
+                </iframe></div>
             </div>
             <h4>Unified Scopes</h4>
             <p>Scopes built using a shared infrastructure makes development efficient and result in a uniform user interface. Shown below are various Scopes built from a unified infrastructure visualizing a LTE downlink signal.</p>
--- src/pages/now.js
+++ src/pages/now.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import drivingScenarioDesigner from "../images/drivingScenarioDesigner.png"
+import birdsEyeScope from "../images/birdsEyeScope.png"
 
 const Now = () => (
     <Layout>
@@ -8,12 +10,30 @@ const Now = () => (
         <section class="content">
             <h3>My Current Projects</h3>
             <p>
-                I currently work on designing and implementing simulation and visualization features for automated driving and signal processing fields. Shown below are some of the highlights.
+                I currently work on designing and implementing simulation and visualization features for automated driving and signal processing fields. I am using C++, JavaScript (Dojo, Chromium Embedded Framework), MATLAB, and MATLAB graphics for development. Shown below are some of the highlights.
             </p>
             <h4>Driving Scenario Simulation</h4>
             <p>
-                Driving Scenario Simulation tools enable engineers to design and test their algorithms in MATLAB and Simulink. Key features include: Lidar point cloud generation; low-poly actor meshes; reverse motion in driving scenarios; road network data model for geographic maps such as HERE/OSM and OpenDRIVE; Driving Scenario Designer app; lanes ground truth specification, visualization, and detection; development of a Driving Scenario Reader block and Bird's-Eye Scope in Simulink.
+                Driving Scenario Simulation tools enable engineers to design and test their algorithms in MATLAB and Simulink. Features I contributed to include: Lidar point cloud generation; low-poly actor meshes; reverse motion in driving scenarios; road network data model for geographic maps such as HERE/OSM and OpenDRIVE; Driving Scenario Designer app; lanes ground truth specification, visualization, and detection; development of a Driving Scenario Reader block and Bird's-Eye Scope in Simulink.
             </p>
+            <h4 class="subHeading">Driving Scenario Designer</h4>
+            <p>
+                Create virtual driving scenarios using the Driving Scenario Designer App.
+            </p>
+            <div class="highlights">
+                <div><img src={drivingScenarioDesigner} alt="Driving Scenario Designer" /></div>
+                <div><iframe src="https://www.youtube.com/embed/cBz9Q7-vNIE" title="Driving Scenario Designer">
+                </iframe></div>
+            </div>
+            <h4 class="subHeading">Bird's-Eye Scope</h4>
+            <p>
+                Visualize roads, actors, sensor detections, and tracks using the Bird's-Eye Scope in Simulink.
+            </p>
+            <div class="highlights">
+                <div><img src={birdsEyeScope} alt="Bird's-Eye Scope" /></div>
+                <div><iframe src="https://www.youtube.com/embed/DcXZrbsBAJM" title="Bird's-Eye Scope">
+                </iframe></div>
+            </div>
         </section>
     </Layout>
 )
